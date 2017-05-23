bf459368dfa8ccaa890680e135e13a2728eca306
Enrico B <rflnrclws@protonmail.com>
2017-05-23 19:22:50 -0400

    Add linter

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "extends": "airbnb",
+    "plugins": [
+        "react",
+        "jsx-a11y",
+        "import"
+    ],
+    "env": {
+        "browser": true,
+    },
+    "rules": {
+        "react/jsx-filename-extension": 0,
+    }
+};
